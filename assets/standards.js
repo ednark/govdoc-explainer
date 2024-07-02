c08f1ff96615cec865d9de18d69dbdf7560d7349
ednark <ednark@gmail.com>
2024-07-02 12:18:49 -0400

    default open overall summary

--- assets/standards.js
+++ assets/standards.js
@@ -1,13 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
+    function toggleAccordion( button ) {
+        button.classList.toggle('active');
+        const content = button.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    }
     document.querySelectorAll('.accordion-header').forEach(button => {
-        button.addEventListener('click', () => {
-            button.classList.toggle('active');
-            const content = button.nextElementSibling;
-            if (content.style.maxHeight) {
-                content.style.maxHeight = null;
-            } else {
-                content.style.maxHeight = content.scrollHeight + "px";
-            }
-        });
+        button.addEventListener('click', () => { toggleAccordion(button) });
     });
+
+    document.querySelectorAll('button').forEach(button => {
+        if (button.textContent.trim() === 'Overall Summary') {
+            toggleAccordion(button)
+        }
+    });
+
 });
